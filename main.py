8718c71445d3ba9a5b5f142966dc4c75a6991f70
tklutey <tlk2129@columbia.edu>
2017-02-14 12:56:50 -0500

    "version1 lyric scraper"

---
+++ main.py
@@ -0,0 +1,36 @@
+import string
+import time
+import re
+import urllib
+def lyrics(artist,song):
+    artist = artist.lower()
+    song = song.lower()
+    artist = re.sub('[^A-Za-z0-9]+', "", artist)
+    song = re.sub('[^A-Za-z0-9]+', "", song)
+    request = "http://azlyrics.com/lyrics/"+str(artist)+"/"+str(song)+".html"
+    print request
+    raw_html = urllib.urlopen(request)
+    html_copy = str(raw_html.read())
+    split = html_copy.split('<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->', 1);
+    words = split[1].split('<!-- MxM banner -->');
+    split = words[0].split('</div>')
+    lyrics = split[0]
+    lyrics = re.sub('(<.*?>)',"",lyrics)
+    return lyrics
+
+def convert_query(input):
+    input.replace(" ", "")
+    input.replace(".", "")
+    input.replace("-","")
+    input.replace("'", "")
+    input = input.lower()
+    return input
+
+def main():
+    artist = convert_query("Run-D.M.C.")
+    song = convert_query("It's Tricky")
+    body = lyrics(artist, song)
+    print body
+
+if __name__ == "__main__":
+    main()
