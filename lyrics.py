47c205796037bf392115dfada16cd2185f4f3842
tklutey <tlk2129@columbia.edu>
2017-04-16 17:45:51 -0400

    "added timeout min"

--- lyrics.py
+++ lyrics.py
@@ -13,7 +13,7 @@ def lyrics(input_artist,input_song):
     try:
         request = "http://azlyrics.com/lyrics/"+str(artist)+"/"+str(song)+".html"
         print request
-        raw_html = urllib2.urlopen(request)
+        raw_html = urllib2.urlopen(request, timeout=10)
         html_copy = str(raw_html.read())
         split = html_copy.split('<!-- Usage of azlyrics.com content by any third-party '
                                 'lyrics provider is prohibited by our licensing agreement. Sorry about that. -->', 1);
