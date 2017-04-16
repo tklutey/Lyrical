50f6be51134d36f90115f4ceca58a017035a80e1
tklutey <tlk2129@columbia.edu>
2017-04-16 18:06:40 -0400

    "reverted to original"

--- lyrics.py
+++ lyrics.py
@@ -13,9 +13,7 @@ def lyrics(input_artist,input_song):
     try:
         request = "http://azlyrics.com/lyrics/"+str(artist)+"/"+str(song)+".html"
         print request
-        # raw_html = urllib2.urlopen(request, timeout=10)
-        print urllib2.urlopen(request).getCode()
-        raw_html = "xyz"
+        raw_html = urllib2.urlopen(request)
         html_copy = str(raw_html.read())
         split = html_copy.split('<!-- Usage of azlyrics.com content by any third-party '
                                 'lyrics provider is prohibited by our licensing agreement. Sorry about that. -->', 1);
